2fa6df30ae9f29596536a2e4e24bb2039d692ca2
WuZheng42 <2381054058@qq.com>
2023-11-16 23:15:52 +0800

    test1

--- proj2/byog/Core/Game.java
+++ proj2/byog/Core/Game.java
@@ -12,7 +12,7 @@ public class Game {
     /**
      * Method used for playing a fresh game. The game should start from the main menu.
      */
-    public void playWithKeyboard() {
+    public void playWithKeyboard() {a
     }
 
     /**
